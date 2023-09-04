2d1eaf71cc8df25cc359240dc5ecce3fa21741ef
jay <9822jaykatwate@gmail.com>
2023-09-04 16:18:52 +0530

    pipes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
 import { Assignment1Component } from './assignment1/assignment1.component';
 import { Assignment2ngmodelchangeComponent } from './assignment2ngmodelchange/assignment2ngmodelchange.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     Assignment1Component,
-    Assignment2ngmodelchangeComponent
+    Assignment2ngmodelchangeComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
